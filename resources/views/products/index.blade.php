12043698a61812cc0aa30a98312bd61a6bcd3a7e
alizarazot <alizarazot@ufpso.edu.co>
2024-04-11 16:15:34 -0500

    add table, but is missing the index, check the js

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+
 @section('content')
     <div class=content-wrapper>
         <!-- Content Header (Page header) -->
@@ -28,7 +29,7 @@
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body">
-                                <table id="example2" class="table table-bordered table-hover">
+                                <table id="products-table" class="table table-bordered table-hover">
                                     <thead>
                                         <tr>
                                             <th>Indentifier</th>
@@ -47,6 +48,22 @@
                                                 <td>{{ $product->image }}</td>
                                                 <td>{{ $product->price }}</td>
                                                 <td>{{ $product->quantity }}</td>
+                                                <td>
+                                                    {{-- @can('products.edit') --}}
+                                                    <a href="{{ route('products.edit', $product->id) }}"
+                                                        class="btn btn-info btn-sm" title=Edit>
+                                                        <i class="fas fa-pencil-alt"></i>
+                                                    </a>
+                                                    {{-- @endcan --}}
+                                                    <form class="d-inline-delete-form"
+                                                        action="{{ route('products.destroy', $product) }}" method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="btn btn-danger btn-sm" title="Delete">
+                                                            <i class="fas fa-trash-alt"></i>
+                                                        </button>
+                                                    </form>
+                                                </td>
                                             </tr>
                                         @endforeach
                                     </tbody>
@@ -62,15 +79,57 @@
                                     </tfoot>
                                 </table>
                             </div>
-                            <!-- /.card-body -->
+                            <!-- /.card -->
                         </div>
-                        <!-- /.card -->
+
                     </div>
                     <!-- /.col -->
                 </div>
                 <!-- /.row -->
+
             </div>
             <!-- /.container-fluid -->
         </section>
     </div>
+
+    @push('scripts')
+        <script type="text/javascript">
+            $(function() {
+                $("#products-table").DataTable({
+                    "responsive": true,
+                    "lengthChange": true,
+                    "autoWidth": false,
+                    //"buttons": ["excel", "pdf", "print", "colvis"],
+                    "language": {
+                        "sLengthMenu": "Mostrar MENU entradas",
+                        "sEmptyTable": "No hay datos disponibles en la tabla",
+                        "sInfo": "Mostrando START a END de TOTAL entradas",
+                        "sInfoEmpty": "Mostrando 0 a 0 de 0 entradas",
+                        "sSearch": "Buscar:",
+                        "sZeroRecords": "No se encontraron registros coincidentes en la tabla",
+                        "sInfoFiltered": "(Filtrado de MAX entradas totales)",
+                        "oPaginate": {
+                            "sFirst": "Primero",
+                            "sPrevious": "First",
+                            "sNext": "Last",
+                            "sLast": "Ultimo"
+                        },
+                        /*"buttons": {
+                            "print": "Imprimir",
+                            "colvis": "Visibilidad Columnas"
+                            /*"create": "Nuevo",
+                            "edit": "Cambiar",
+                            "remove": "Borrar",
+                            "copy": "Copiar",
+                            "csv": "fichero CSV",
+                            "excel": "tabla Excel",
+                            "pdf": "documento PDF",
+                            "collection": "Colección",
+                            "upload": "Seleccione fichero...."
+                        }*/
+                    }
+                }); //.buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+            });
+        </script>
+    @endpush
 @endsection
