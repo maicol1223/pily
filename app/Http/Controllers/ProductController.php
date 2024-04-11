1b388280eb3118e2d024f49c33bb15bc6f0996e6
alizarazot <alizarazot@ufpso.edu.co>
2024-04-11 15:23:54 -0500

    render products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+
 class ProductController extends Controller
 {
     /**
@@ -11,7 +13,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return view("products.index");
+        $products = Product::all();
+        return view("products.index", compact("products"));
     }
 
     /**
