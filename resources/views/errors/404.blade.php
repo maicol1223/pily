79e9838aa91adba94f68a483cd0b76c26f8581a1
alizarazot <alizarazot@ufpso.edu.co>
2024-04-24 14:25:19 -0500

    Tweak 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,42 @@
-Error
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page not found | 404 </title>
+
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            box-sizing: border-box;
+            padding: 30px;
+        }
+
+        h1,
+        h2 {
+            font-family: sans-serif;
+            margin: 20px;
+            user-select: none;
+        }
+
+        h1 {
+            font-size: 90px;
+        }
+
+        h2 {
+            font-size: 70px;
+            font-weight: bolder;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Page not found.</h1>
+    <h2>(≥o≤)</h2>
+    <img src="{{ asset('assets/cow-ufo.gif') }} ">
+</body>
+
+</html>
